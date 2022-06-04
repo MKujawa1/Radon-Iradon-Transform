2eadd53c1737e2f2b1e0c9ab149b7dd3f8d78e76
MKujawa1 <maciej.kujawa96@o2.pl>
2022-06-04 16:47:48 +0200

    Increase speed of radon. Fixed bug with amlitude

--- functions.py
+++ functions.py
@@ -26,29 +26,30 @@ def circle_mask(y,x):
 
 def radon_transform(img,n_angles):
     '''
-    Creating sinogram from 2d image.
+    Creating sinogram from image
 
     Parameters
     ----------
     img : array
-        input 2d image.
+        2d image.
     n_angles : int
-        values of angles.
+        number of angles.
 
     Returns
     -------
     sinogram : array
-        returns 2d sinogram.
+        sinogram from image.
 
     '''
+    sinogram = np.zeros((len(img),n_angles))
     min_angle = 180/2/n_angles
     max_angle = 180-180/2/n_angles
     theta = np.linspace(min_angle,max_angle,n_angles)
-    sinogram = np.zeros((len(img),n_angles))
     for i in range(np.amin(np.shape(sinogram))):
         rot_image = rotate(img,theta[i])
-        sinogram[:,i] = np.flip(np.squeeze(resize(rot_image,(1,len(img)))))
-    return sinogram
+        sinogram[:,i] = sum(rot_image)
+    sinogram = np.fliplr(np.flipud(sinogram))
+    return sinogram 
 
 def iradon_transform(sinogram,n_angles):
     '''
